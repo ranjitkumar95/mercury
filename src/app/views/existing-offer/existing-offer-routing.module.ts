f747d4b66975ada760a071b0ef4a7f626133dd1c
ranjitkumarray25 <ranjitkumar@digitalway-lu.com>
2021-11-06 22:25:03 +0530

    new change

--- src/app/views/existing-offer/existing-offer-routing.module.ts
+++ src/app/views/existing-offer/existing-offer-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from 'src/app/layout';
 import { ExistingOfferComponent } from './existing-offer.component';
 
-const routes: Routes = [{ path: '', component: ExistingOfferComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      { path: '', component: ExistingOfferComponent }]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/views/existing-order/existing-order-routing.module.ts
+++ src/app/views/existing-order/existing-order-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from 'src/app/layout';
 import { ExistingOrderComponent } from './existing-order.component';
 
-const routes: Routes = [{ path: '', component: ExistingOrderComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children: [
+      { path: '', component: ExistingOrderComponent }
+    ]
+  }]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
